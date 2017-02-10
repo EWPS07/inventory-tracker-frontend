be24d372a389a8a927e5a3d52a56e68d22262dc2
Evan Paige <ewpaige@gmail.com>
2017-02-10 14:15:27 -0800

    initial commit

---
+++ app/entry.js
@@ -0,0 +1,55 @@
+'use strict';
+
+require('./scss/main.scss');
+
+const path = require('path');
+const angular = require('angular');
+const camelcase = require('camelcase');
+const pascalcase = require('pascalcase');
+const uiRouter = require('angular-ui-router');
+const ngTouch = require('angular-touch');
+const ngAnimate = require('angular-animate');
+const uiBootstrap = require('angular-ui-bootstrap');
+const ngFileUpload = require('ng-file-upload');
+
+const inventory_tracker = angular.module('inventory_tracker', [ngTouch, ngAnimate, uiRouter, uiBootstrap, ngFileUpload]);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys().forEach( path => {
+  inventory_tracker.config(context(path));
+});
+
+context = require.context('./view/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = pascalcase(path.basename(key, '.js'));
+  let module = context(key);
+  inventory_tracker.controller(name, module);
+});
+
+context = require.context('./service/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  inventory_tracker.service(name, module);
+});
+
+context = require.context('./component/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  inventory_tracker.component(name, module);
+});
+
+context = require.context('./filter/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  inventory_tracker.filter(name, module);
+});
+
+context = require.context('./directive/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  inventory_tracker.directive(name, module);
+});
