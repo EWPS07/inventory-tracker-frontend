7c550fcdf2ed263c27c7a9515b9a4c9fc90b1e12
Hawa Abdi <abdih17@hotmail.com>
2017-02-13 16:06:19 -0800

    Initial functionality of cart product services.

---
+++ app/service/cart-product-service.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = ['$q', '$log', '$http', 'storeService', 'cartOrderService', cartProductService];
+
+function cartProductService($q, $log, $http, authService, customerService, storeService, cartOrderService) {
+  $log.debug('cartProductService()');
+
+  let service = {};
+
+  let url = `${__API_URL__}/api`;
+  let config = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    }
+  };
+
+  service.createCartProduct = function(cartOrderID, storeID) {
+    $log.debug('cartProductService.createCartProduct()');
+
+    return $http.post(`${url}/orders/${cartOrderID}/${storeID}/cart`, config)
+    .catch( err => {
+      $log.error(err.message);
+    });
+  };
+
+  service.putCartProduct = function(productID) {
+    $log.debug('cartProductService.putCartProduct()');
+
+    return $http.put(`${__API_URL__}/products/${productID}`, config)
+    .then(res => $q.resolve(res.data))
+    .catch(err => $log.error(err.message));
+  };
+
+  service.deleteOrder = function(productID) {
+    $log.debug('cartProductService.deleteOrder()');
+
+    return $http.delete(`${url}/products/${productID}`, config)
+    .catch(err => $log.error(err.message));
+  };
+}
