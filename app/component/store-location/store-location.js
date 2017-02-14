f23b379f6e360d3f02d3f40cca612714f4c033eb
Steven Bateman <batemanvo@gmail.com>
2017-02-14 12:19:45 -0800

    Initial logic for StoreLocationController.

--- app/component/store-location/store-location.js
+++ app/component/store-location/store-location.js
@@ -1,1 +1,27 @@
 'use strict';
+
+module.exports = {
+  template: require('./store-location.html'),
+  controller: ['$log', 'storeService', 'customerService', StoreLocationController],
+  controllerAs: 'storeLocationCtrl'
+};
+
+function StoreLocationController($log, storeService, customerService) {
+  storeService.getStores();
+
+  if (storeService.stores) {
+    this.currentStore = storeService.stores[0];
+    this.allStores = storeService.stores;
+  }
+
+  if (customerService.currentCustomer.favoriteStore) {
+    this.currentStore = storeService.stores.find(_store => _store.storeNumber === customerService.currentCustomer.favoriteStore);
+  }
+
+  this.changeStore = function(storeID) {
+    storeService.getStore(storeID)
+    .then(_store => this.currentStore = _store);
+  };
+
+
+}
