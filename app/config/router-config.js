6747e7aabf76b6aa41ceeeeb71ab64c9b280ef0f
Steven Bateman <batemanvo@gmail.com>
2017-02-14 13:17:54 -0800

    Removed comments.

--- app/config/router-config.js
+++ app/config/router-config.js
@@ -9,20 +9,20 @@ function routerConfig($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.when('/login', '/join#login');
 
   let states = [
-    // {
-    //   name: 'home',
-    //   url: '/home',
-    //   template: require('../view/home/home.html'),
-    //   controller: 'HomeController',
-    //   controllerAs: 'homeCtrl'
-    // },
-    // {
-    //   name: 'landing',
-    //   url: '/join',
-    //   template: require('../view/landing/landing.html'),
-    //   controller: 'LandingController',
-    //   controllerAs: 'landingCtrl'
-    // },
+    {
+      name: 'home',
+      url: '/home',
+      template: require('../view/home/home.html'),
+      controller: 'HomeController',
+      controllerAs: 'homeCtrl'
+    },
+    {
+      name: 'landing',
+      url: '/join',
+      template: require('../view/landing/landing.html'),
+      controller: 'LandingController',
+      controllerAs: 'landingCtrl'
+    },
     {
       name: 'shopping',
       url: '/shopping',
