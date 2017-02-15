32d7833c64a38af6d1c05842ca8deb8e58ed61f9
Steven Bateman <batemanvo@gmail.com>
2017-02-15 14:40:52 -0800

    Added current order controller and get/change methods.

--- app/component/cart-view/current-order/current-order.js
+++ app/component/cart-view/current-order/current-order.js
@@ -1,1 +1,20 @@
 'use strict';
+
+module.exports = {
+  template: require('./current-order.html'),
+  controller: ['$log', 'customerService', CurrentOrderController],
+  controllerAs: 'currentOrderCtrl'
+};
+
+function CurrentOrderController($log, customerService) {
+  $log.log('Current order controller');
+
+  this.getOrders = function() {
+    this.customerOrders = customerService.currentCustomer.currentOrders.filter(_order => !_order.completed).reverse();
+    this.currentOrder = this.customerOrders[0];
+  };
+
+  this.changeOrder = function(orderID) {
+    this.currentOrder = this.customerOrders.find(_order => _order._id === orderID);
+  };
+}
