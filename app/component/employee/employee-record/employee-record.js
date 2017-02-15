52b64ae6044a6348877bb5357bb62db5c3c9321c
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-14 19:56:54 -0800

    build out employee record component controller

--- app/component/employee/employee-record/employee-record.js
+++ app/component/employee/employee-record/employee-record.js
@@ -2,4 +2,21 @@
 
 require('./_employee-record.scss');
 
-// TODO: BUILD OUT EMPLOYEE RECORD COMPONENT
+module.exports = {
+  template: require('./employee-record.html'),
+  controller: ['$log', 'employeeService', EmployeeRecordController],
+  controllerAs: 'employeeRecordCtrl',
+  bindings: {
+    employee: '<'
+  }
+};
+
+function EmployeeRecordController($log, employeeService) {
+  $log.debug('EmployeeRecordController');
+
+  this.showEditEmployeeRecord = false;
+
+  this.deleteEmployeeRecord = function() {
+    employeeService.removeEmployee(this.employee._id);
+  };
+};
