9d6ed8db118789a15caf23ec87369f4056730113
Evan Paige <ewpaige@gmail.com>
2017-02-14 16:09:07 -0800

    adding files for settings-view-components

--- app/service/inventory-product-service.js
+++ app/service/inventory-product-service.js
@@ -1,42 +1,54 @@
 'use strict';
 
-module.exports = ['$q', '$log', '$http', inventoryProductService];
+module.exports = ['$q', '$log', '$http', 'storeService', inventoryProductService];
 
-function inventoryProductService($q, $log, $http) {
+function inventoryProductService($q, $log, $http, storeService) {
 
   let service = {};
+  let config = {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  };
+  service.currentInventory = storeService.currentStore.current;
 
-  // ADD NEW PRODUCT TO CURRENT INVENTORY
-  service.addNewProduct = function(product) {
+  // ADD NEW PRODUCT TO CURRENT INVENTORY --------------------------------------
+  service.addNewProduct = function(product, storeID) {
     $log.debug('inventoryProductService.addNewProduct()');
 
     let url = `${__API_URL__}/api/store/${storeID}/inventory`;
 
     return $http.post(url, product, config)
+    .then( res => service.currentInventory.push(res.data))
     .catch( err => $log.error(err.message));
   };
 
-  // ADD PRODUCT TO INVENTORY ORDER
-  service.addProductToInventoryOrder = function(product) {
+  // ADD PRODUCT TO INVENTORY ORDER --------------------------------------------
+  service.addProductToInventoryOrder = function(product, inventoryOrderID) {
     $log.debug('inventoryProductService.addProductToInventoryOrder()');
 
     let url = `${__API_URL__}/api/inventory-orders/${inventoryOrderID}/inventory`;
 
     return $http.post(url, product, config)
+    .then(res => res.data)
     .catch( err => $log.error(err.message));
   };
 
-  // ADD INVENTORY ORDER TO CURRENT INVENTORY
+  // ADD INVENTORY ORDER TO CURRENT INVENTORY ----------------------------------
   service.addInventoryOrderToCurrentInventory = function(inventoryOrderID) {
     $log.debug('inventoryProductService.addInventoryOrderToCurrentInventory()');
 
     let url = `${__API_URL__}/api/inventory-orders/${inventoryOrderID}/complete-order`;
 
     return $http.post(url, config)
+    .then( res =>
+      service.currentInventory.push(res.data)
+    )
     .catch(err => $log.error(err.message));
   };
 
-  // UPDATE INVENTORY PRODUCT
+  // UPDATE INVENTORY PRODUCT --------------------------------------------------
   service.updateProduct = function(product, inventoryID) {
     $log.debug('inventoryProductService.updateProduct()');
 
@@ -46,15 +58,29 @@ function inventoryProductService($q, $log, $http) {
     .catch(err => $log.error(err.message));
   };
 
-  // DELETE PRODUCT FROM INVENTORY
-  service.deleteProduct = function() {
+  // DELETE PRODUCT FROM UI ----------------------------------------------------
+  service.deleteFromUI = function(arr, id) {
+    for(let i=0; i<arr.length; i++) {
+      if(arr[i]._id === id) {
+        arr.splice([i], 1);
+        break;
+      }
+    }
+    return arr;
+  };
+
+  // DELETE PRODUCT FROM INVENTORY ---------------------------------------------
+  service.deleteProduct = function(inventoryID) {
     $log.debug('inventoryProductService.deleteProduct()');
 
-    let url = `${__API_URL_}/api/inventory/${inventoryID}`;
+    let url = `${__API_URL__}/api/inventory/${inventoryID}`;
 
     return $http.delete(url, config)
+    .then(
+      service.deleteFromUI(service.currentInventory, inventoryID)
+    )
     .catch(err => $log.error(err.message));
   };
 
   return service;
-};
+}
