59de7eb6feeb3f7fe7a9b1302d87039cf6be1dc8
Hawa Abdi <abdih17@hotmail.com>
2017-02-16 15:07:32 -0800

    Initial submission of updating and adding orders into a store.

---
+++ app/component/inventory-order/inventory-order.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  template: require('./inventory-order.html'),
+  controller: ['$log', 'storeService', 'inventoryOrderService', 'inventoryProductService', InventoryOrderController],
+  controllerAs: 'inventoryOrderCtrl'
+};
+
+function InventoryOrderController($log, storeService, inventoryOrderService, inventoryProductService){
+  $log.log('InventoryOrderController');
+
+  this.storeService = storeService;
+
+  this.createOrder = function() {
+    return new Promise((resolve) => {
+      let lastOrder = storeService.currentStore.incoming[storeService.currentStore.incoming.length-1];
+
+      if(lastOrder && !lastOrder.completed) {
+        inventoryOrderService.getOrder(lastOrder._id)
+        .then(_order => {
+          this.currentOrder = _order;
+          inventoryOrderService.currentOrder = this.currentOrder;
+          return resolve();
+        });
+      }
+
+      inventoryOrderService.addInventoryOrder(storeService.currentStore._id)
+      .then( order => {
+        this.currentOrder = order;
+        inventoryOrderService.currentOrder = this.currentOrder;
+        return resolve();
+      });
+    });
+  };
+
+  this.addToOrder = function(product) {
+    this.createOrder()
+    .then(() => {
+      $log.log(inventoryOrderService.currentOrder);
+      let updateProduct = this.currentOrder.inventories.find(_product => _product.desc === product.desc);
+
+      if(updateProduct){
+        updateProduct.quantity += updateProduct.buyQuantity;
+        inventoryProductService.updateProduct(updateProduct, inventoryOrderService.currentOrder._id);
+        return;
+      }
+      product.quantity = product.buyQuantity;
+      inventoryProductService.addProductToInventoryOrder(product, this.currentOrder._id);
+    });
+  };
+}
