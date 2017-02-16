a0f9e389b89a97cbad645f9760f1110aacd59e68
Steven Bateman <batemanvo@gmail.com>
2017-02-15 23:20:41 -0800

    The cart page now properly loads products on all visits.

--- app/component/cart-view/current-order/current-order.js
+++ app/component/cart-view/current-order/current-order.js
@@ -2,26 +2,34 @@
 
 module.exports = {
   template: require('./current-order.html'),
-  controller: ['$log', '$rootScope', 'customerService', CurrentOrderController],
+  controller: ['$log', '$rootScope', 'customerService', 'cartOrderService', CurrentOrderController],
   controllerAs: 'currentOrderCtrl'
 };
 
-function CurrentOrderController($log, $rootScope, customerService) {
+function CurrentOrderController($log, $rootScope, customerService, cartOrderService) {
   $log.log('Current order controller');
 
   this.getOrders = function() {
     this.customerOrders = customerService.currentCustomer.currentOrders.filter(_order => !_order.completed).reverse();
     this.currentOrder = customerService.currentCustomer.currentOrders.filter(_order => !_order.completed).reverse()[0]._id;
     customerService.currentOrder = this.customerOrders[0];
-    customerService.currentTotal = 0;
-    customerService.currentOrder.products.forEach(product => customerService.currentTotal += (product.quantity * product.price));
+    cartOrderService.getOrder(customerService.currentOrder._id)
+    .then(order => {
+      customerService.currentOrder.products = order.products;
+      customerService.currentTotal = 0;
+      customerService.currentOrder.products.forEach(product => customerService.currentTotal += (product.quantity * product.price));
+    });
   };
 
   this.changeOrder = function(orderID) {
     this.currentOrder = customerService.currentCustomer.currentOrders.find(_order => _order._id === orderID)._id;
     customerService.currentOrder = customerService.currentCustomer.currentOrders.find(_order => _order._id === orderID);
-    customerService.currentTotal = 0;
-    customerService.currentOrder.products.forEach(product => customerService.currentTotal += (product.quantity * product.price));
+    cartOrderService.getOrder(customerService.currentOrder._id)
+    .then(order => {
+      customerService.currentOrder.products = order.products;
+      customerService.currentTotal = 0;
+      customerService.currentOrder.products.forEach(product => customerService.currentTotal += (product.quantity * product.price));
+    });
   };
 
   this.getOrders();
