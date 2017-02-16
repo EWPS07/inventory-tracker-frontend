6afcd089900abf30a8695e33641bc4b7ef4036c4
Hawa Abdi <abdih17@hotmail.com>
2017-02-16 11:39:59 -0800

    Added edit functionality to current products

--- app/component/inventory/current/current.js
+++ app/component/inventory/current/current.js
@@ -2,12 +2,28 @@
 
 module.exports = {
   template: require('./current.html'),
-  controller: ['$log', 'storeService', CurrentController],
+  controller: ['$log', 'storeService', 'inventoryProductService', CurrentController],
   controllerAs: 'currentCtrl'
 };
 
-function CurrentController($log, storeService) {
+function CurrentController($log, storeService, inventoryProductService) {
   $log.log('CurrentController');
 
   this.storeService = storeService;
+  this.editProduct = function(product) {
+    product.name = product.newName;
+    product.desc = product.newDesc;
+    product.quantity = product.newQuantity;
+    product.category = product.newCategory;
+    product.price = product.price;
+    inventoryProductService.updateProduct(product, product._id)
+    .then(() => {
+      for(var i = 0; i < storeService.currentStore.current.length; i++){
+        if( storeService.currentStore.current[i]._id === product._id ){
+          storeService.currentStore.current[i] = product;
+          break;
+        }
+      }
+    });
+  };
 }
