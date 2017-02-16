65123b6e1c7a7c977d3764145ce6050e13df42a1
Caleb Sattgast <calebsattgast@gmail.com>
2017-02-15 16:57:31 -0800

    build out basics of add employee component, include TODOs

--- app/component/employee/add-employee/add-employee.js
+++ app/component/employee/add-employee/add-employee.js
@@ -3,3 +3,22 @@
 require('./_add-employee.scss');
 
 // TODO: BUILD OUT ADD EMPLOYEE RECORD COMPONENT
+module.exports = {
+  template: require('./add-employee.html'),
+  controller: ['$log', 'employeeService', AddEmployeeController],
+  controllerAs: 'addEmployeeCtrl'
+};
+
+function AddEmployeeController($log, employeeService) {
+  $log.debug('AddEmployeeController');
+
+  this.employee = {};
+
+  this.addEmployeeRecord = function() {
+    employeeService.addEmployeeAsAdmin(this.employee)
+    .then( () => {
+      this.employee.name = null;
+      this.employee.desc = null;
+    });
+  };
+};
