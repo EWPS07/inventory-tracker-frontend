f988be1961a97df5abaae99a3037ec0cf22afe11
Evan Paige <ewpaige@gmail.com>
2017-03-08 12:42:33 -0800

    employee-dashboard-flexbox-refactor-incomplete

--- app/component/employee-landing/employee-signup/employee-signup.js
+++ app/component/employee-landing/employee-signup/employee-signup.js
@@ -4,11 +4,11 @@ require('./_employee-signup.scss');
 
 module.exports = {
   template: require('./employee-signup.html'),
-  controller: ['$log', '$location', 'employeeService', EmployeeSignupController],
+  controller: ['$log', '$location', 'employeeService', '$window', EmployeeSignupController],
   controllerAs: 'employeeSignupCtrl'
 };
 
-function EmployeeSignupController($log, $location, employeeService) {
+function EmployeeSignupController($log, $location, employeeService, $window) {
   $log.debug('EmployeeSignupController');
 
   employeeService.getToken()
@@ -16,12 +16,16 @@ function EmployeeSignupController($log, $location, employeeService) {
     $location.url('/employee');
   });
 
+  this.verifyPassword = '';
+
   this.signup = function(employee) {
     $log.debug('signupControl.signup()');
-
-    employeeService.registerEmployee(employee)
-    .then( () => {
-      $location.url('/employee');
-    });
+    if(this.verifyPassword === this.employee.password) {
+      return employeeService.registerEmployee(employee)
+      .then( () => {
+        $location.url('/employee');
+      });
+    }
+    $window.alert('You must confirm your password');
   };
 }
